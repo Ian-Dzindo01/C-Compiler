d9b38d4a66e153bc63f1df750179423b1781f84c
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-01-17 18:51:36 +0100

    Add 2 letter lexeme support

--- Scanner.java
+++ Scanner.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import laga.Laga;
+
 import static laga.TokenType.*
 
 class Scanner {
@@ -58,6 +61,31 @@ class Scanner {
           case '+': addToken(PLUS); break;
           case ';': addToken(SEMICOLON); break;
           case '*': addToken(STAR); break; 
+
+          case '!':
+          addToken(match('=') ? BANG_EQUAL : BANG); // "If the current character is '=', add a token of type BANG_EQUAL; otherwise, add a token of type BANG."
+          break;
+        case '=':
+          addToken(match('=') ? EQUAL_EQUAL : EQUAL); // same onwards
+          break;
+        case '<':
+          addToken(match('=') ? LESS_EQUAL : LESS);
+          break;
+        case '>':
+          addToken(match('=') ? GREATER_EQUAL : GREATER);
+          break;
+
+          default:
+          Laga.error(line, "Unexpected character.");  // handling all other types of unknown characters
+          break;
+        }
+
+        private boolean match(char expected) {
+            if (isAtEnd()) return false;
+            if (source.charAt(current) != expected) return false;
+
+            current++;
+            return true;
         }
       }
 
