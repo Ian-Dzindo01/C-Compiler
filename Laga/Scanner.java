67e5bede2b1adfe9a8093a099e99ef0d3ec882bb
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-01-20 12:02:14 +0100

    Fix Visitor mispell error

--- Laga/Scanner.java
+++ Laga/Scanner.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import Laga.Laga;
+//import Laga.Laga;
 
 import static Laga.TokenType.*;   // bad practice, but works perfect for this example
 
